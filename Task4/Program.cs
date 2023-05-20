8209a214886646687540b094be681930ce74f569
Boris <89241300089@mail.ru>
2023-05-20 15:07:29 +0900

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+class Program {
+    public static void Main (string[] args) {
+        Console.Write("Input any number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i <= number; i+=2)
+        {
+            Console.Write(i);
+        }
+     }
+}
